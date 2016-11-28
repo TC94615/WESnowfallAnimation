dd4b7d08a7c8a2c0233d82d428c686eb5aac014f
TC94615 <tclee0117@gmail.com>
2016-11-28 17:08:18 +0800

    Initial Commit

---
+++ snowfall/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  snowfall
+//
+//  Created by wish8 on 28/11/2016.
+//  Copyright © 2016 osolve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ snowfallTests/snowfallTests.m
@@ -0,0 +1,39 @@
+//
+//  snowfallTests.m
+//  snowfallTests
+//
+//  Created by wish8 on 28/11/2016.
+//  Copyright © 2016 osolve. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface snowfallTests : XCTestCase
+
+@end
+
+@implementation snowfallTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
